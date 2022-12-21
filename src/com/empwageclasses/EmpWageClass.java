460eb474e18c5d7bc1fad77216a2bf5e5093826e
Saipavanganesh <pavanganeshms@gmail.com>
2022-12-22 03:07:45 +0530

    first commit

---
+++ src/com/empwageclasses/EmpWageClass.java
@@ -0,0 +1,8 @@
+package com.empwageclasses;
+
+public class EmpWageClass {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+        
+    }
+}
