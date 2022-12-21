1a0c0ae3f636fa5efa6fc0447e42330a0e20ee66
Saipavanganesh <pavanganeshms@gmail.com>
2022-12-22 03:09:20 +0530

    checking attendance

--- src/com/empwageclasses/EmpWageClass.java
+++ src/com/empwageclasses/EmpWageClass.java
@@ -3,6 +3,14 @@ package com.empwageclasses;
 public class EmpWageClass {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        double empCheck=Math.floor(Math.random()*10)%2;
+
+        if(empCheck==1.0){
+            System.out.println("Employee is present");
+        }
+        else{
+            System.out.println("Employee is absent");
+        }
         
     }
 }
