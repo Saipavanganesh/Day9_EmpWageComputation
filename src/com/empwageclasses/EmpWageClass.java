bf43e498031985229378ac62484e79d2b0aea3b6
Saipavanganesh <pavanganeshms@gmail.com>
2022-12-22 03:13:18 +0530

    part time

--- src/com/empwageclasses/EmpWageClass.java
+++ src/com/empwageclasses/EmpWageClass.java
@@ -4,7 +4,7 @@ public class EmpWageClass {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        double empCheck=Math.floor(Math.random()*10)%2;
+        double empCheck=Math.floor(Math.random()*10)%3;
         int wagePerHour=20;
         int fullDayHours=8;
         int salary;
@@ -12,10 +12,15 @@ public class EmpWageClass {
         if(empCheck==1.0){
             System.out.println("Employee is present");
         }
+        else if (empCheck==2.0) {
+            System.out.println("Emp is Part time present");
+            fullDayHours=4;
+        }
         else{
             System.out.println("Employee is absent");
             fullDayHours=0;
         }
+
         salary=fullDayHours*wagePerHour;
         System.out.println("Daily Wage is "+salary);
 
